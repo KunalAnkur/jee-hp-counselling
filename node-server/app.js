b6f76630e1241576058e743025b0e772c9374498
Yash priyam <yashpriyam74@gmail.com>
2021-09-19 09:39:34 +0530

    change order

--- node-server/app.js
+++ node-server/app.js
@@ -3,21 +3,16 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 require("dotenv").config();
 const mongoose = require("mongoose");
+
+
 const {
   getCollegeData,
   getCollegeDataFiltering,
 } = require("./controllers/college-data-controller");
-
 const {counsellingSix} = require("./db-models/collegeData");
-
 const {couselling_six_data} = require("./couselling_six");
 
 const app = express();
-
-// const corsOptions = {
-//   origin: ['https://jee-counselling.netlify.app/', 'https://hp-jee-server.herokuapp.com/'],
-//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-// }
 
 app.use(express.json());
 app.use(cookieParser());
@@ -29,57 +24,43 @@ app.use(
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
 
-let dbConnector = "";
-const fileName = "Counsel-six.csv";
-const arrayToInsert = [];
+// let dbConnector = "";
+// const fileName = "Counsel-six.csv";
+// const arrayToInsert = [];
 
 const MONGODB_URI = process.env.DEV_DB;
 // process.env.NODE_ENV === 'production' ? process.env.PROD_DB : process.env.DEV_DB
-mongoose
-  .connect(`${MONGODB_URI}`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then((client) => {
-    dbConnector = client.connections[0].db;
-    console.log("database is connected");
-    // uncommenting this line will re-upload data to db
-    // addCsvDataToMongoAsJson();
-  })
-  .catch((err) => {
-    console.log(err);
-  });
 
-async function addCsvDataToMongoAsJson() {
-  // return csvtojson()
-  //   .fromFile(fileName)
-  //   .then((source) => {
-  // Fetching the all data from each row
-  const source = couselling_six_data;
-  for (let i = 0; i < source.length; i++) {
-    let oneRow = {
-      institute: source[i]["Institute"],
-      academic_program_name: source[i]["Academic Program Name"],
-      quota: source[i]["Quota"],
-      seat_type: source[i]["Seat Type"],
-      gender: source[i]["Gender"],
-      opening_rank: source[i]["Opening Rank"],
-      closing_rank: source[i]["Closing Rank"],
-    };
-    arrayToInsert.push(oneRow);
-  }
-  //inserting into the table “employees”
-  let collectionName = "couselling-six-second-tests";
-  let collection = dbConnector.collection(collectionName);
-  collection.insertMany(arrayToInsert, (err, result) => {
-    if (err) console.log(err);
-    if (result) {
-      console.log("Import CSV into database successfully.");
-    }
-  });
-  // });
-  return;
-}
+// async function addCsvDataToMongoAsJson(dbConnector) {
+//   // return csvtojson()
+//   //   .fromFile(fileName)
+//   //   .then((source) => {
+//   // Fetching the all data from each row
+//   const source = couselling_six_data;
+//   for (let i = 0; i < source.length; i++) {
+//     let oneRow = {
+//       institute: source[i]["Institute"],
+//       academic_program_name: source[i]["Academic Program Name"],
+//       quota: source[i]["Quota"],
+//       seat_type: source[i]["Seat Type"],
+//       gender: source[i]["Gender"],
+//       opening_rank: source[i]["Opening Rank"],
+//       closing_rank: source[i]["Closing Rank"],
+//     };
+//     arrayToInsert.push(oneRow);
+//   }
+//   //inserting into the table “employees”
+//   let collectionName = "couselling-six-second-tests";
+//   let collection = dbConnector.collection(collectionName);
+//   collection.insertMany(arrayToInsert, (err, result) => {
+//     if (err) console.log(err);
+//     if (result) {
+//       console.log("Import CSV into database successfully.");
+//     }
+//   });
+//   // });
+//   return;
+// }
 
 // app.options('*', cors(corsOptions))
 
@@ -106,9 +87,24 @@ app.get("*", (req, res) => {
   res.send("invalid route");
 });
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server is listening at ${process.env.PORT}`)
-);
+
+mongoose
+  .connect(`${MONGODB_URI}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((client) => {
+    // dbConnector = client.connections[0].db;
+    app.listen(process.env.PORT, () =>
+      console.log(`Server is listening at ${process.env.PORT}`)
+    );
+    console.log("database is connected");
+    // uncommenting this line will re-upload data to db
+    // addCsvDataToMongoAsJson(dbConnector);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // .env file
 // DEV_DB = mongodb+srv://nodoubtapp:Yq6IXnYjTov2ZnQD@cluster0.k468m.mongodb.net/harshpriyam?retryWrites=true&w=majority
